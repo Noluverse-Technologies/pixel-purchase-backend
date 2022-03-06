52929c295793ac96e6248b2450b483edb3bddff9
soumyaRauth <shoummorauth@live.co.uk>
2022-03-07 02:29:49 +0600

    wallet required on registration validation removed

--- Modules/Users/Http/Controllers/AuthController.php
+++ Modules/Users/Http/Controllers/AuthController.php
@@ -60,7 +60,6 @@ class AuthController extends GenericResponseController
             'firstname' => 'required',
             'lastname' => 'required',
             'email' => 'required|email|unique:users',
-            'wallet_address' => 'unique:users',
             'password' => 'required',
             'image' => 'image|mimes:jpg,png,jpeg,gif,svg|max:2048',
             'role' => 'required',
