2a95fec1fafc55b41302ddcf6aaa2a31e5f39287
soumyaRauth <shoummorauth@live.co.uk>
2022-03-13 02:50:38 +0600

    first commit to noluverse technologies repo

--- Modules/Payment/Database/Migrations/2022_03_12_203618_create_transactions_table.php
+++ Modules/Payment/Database/Migrations/2022_03_12_203618_create_transactions_table.php
@@ -11,7 +11,7 @@ class CreateTransactionsTable extends Migration
      *
      * @return void
      */
-    public function up()
+    public function up() 
     {
         Schema::create('transactions', function (Blueprint $table) {
             $table->id();
